6a9ea9cb443fc87f67bffe942592c84a911562d1
@luisedward777 <luisedward.miranda@gmail.com>
2018-04-15 22:51:09 +0800

    Prepared the event handlers on class Transaction

--- Transaction.php
+++ Transaction.php
@@ -6,5 +6,19 @@ use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
 class Transaction implements MessageComponentInterface {
+    public function onOpen(ConnectionInterface $conn) {
 
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        
+    }
 }
