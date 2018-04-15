da51ca86ad78cfe00f1db873b1c1ad85e2e61e37
@luisedward777 <luisedward.miranda@gmail.com>
2018-04-15 23:41:46 +0800

    Optimize the CLI

--- sockjson.php
+++ sockjson.php
@@ -4,34 +4,23 @@ use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 
-if (count($argv) > 0){
-    @ $str1 = $argv[1];
-    @ $str2 = $argv[2];
-    @ $str3 = $argv[3];
-    @ $str4 = $argv[4];
+require 'vendor/autoload.php';
+require 'class/Transaction.php';
 
-    if ($str1 == "start"){
-        
-        require 'vendor/autoload.php';
-        require 'class/Transaction.php';
+$server = IoServer::factory( 
+    new HttpServer(
+        new WsServer(
+            new Transaction()
+        )
+    ), 8080 
+);
 
-        $server = IoServer::factory( 
-            new HttpServer(
-                new WsServer(
-                    new Transaction()
-                )
-            ), 8080 
-        );
-
-        try{
-            echo "Starting the application...\n";
-            echo "Application Running...\n";
-            $server->run();
-        }
-        catch(\RuntimeException $e){
-            echo "Failed to initialize in port for real-time Transaction. A React Loop was not provided during instantiation\n";
-            exit;
-        }
-
-    }
+try{
+    echo "Starting the application...\n";
+    echo "Application Running...\n";
+    $server->run();
+}
+catch(\RuntimeException $e){
+    echo "Failed to initialize in port for real-time Transaction. A React Loop was not provided during instantiation\n";
+    exit;
 }
