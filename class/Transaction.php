eb568dcde38f42529d9de6991c06fc9d8140f845
@luisedward777 <luisedward.miranda@gmail.com>
2018-04-15 23:07:18 +0800

    Added onMessage Algorithm for sending message

--- class/Transaction.php
+++ class/Transaction.php
@@ -15,11 +15,20 @@ class Transaction implements MessageComponentInterface {
          $this->clients->attach($conn);
 
          echo "New connection! ({$conn->resourceId})\n";
-         
+
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
 
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                // The sender is not the receiver, send to each client connected
+                $client->send($msg);
+            }
+        }
     }
 
     public function onClose(ConnectionInterface $conn) {
